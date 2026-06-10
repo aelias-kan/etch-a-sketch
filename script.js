edd5a896fff1981e7e8258a7d319458750dd7aa2
aelias-kan <150896358+aelias-kan@users.noreply.github.com>
2026-06-10 04:46:49 +0400

    create click and drag sketching and fortifyinput validations
    Changed drawing mechanic from mouseover to click-and-hold using a global `isDrawing` flag and
    Fixed canvas vanishing bugs caused by empty inputs, alphabetical characters (`isNaN`), and bounds violations.

--- script.js
+++ script.js
@@ -1,64 +1,85 @@
-// alert("script working");
-const ctrlBtn = document.querySelector('.controls')
-const mainContainer = document.querySelector('.container');
-const length = document.querySelector('.size-input');
-const resizeBtn = document.querySelector('.resize-btn');
-const clearBtn = document.querySelector('.clear-btn'); 
-let currentGridSize =25;
-createGrid(currentGridSize);
+    const mainContainer = document.querySelector('.container');
+    const length = document.querySelector('.size-input');
+    const resizeBtn = document.querySelector('.resize-btn');
+    const clearBtn = document.querySelector('.clear-btn'); 
+    let currentGridSize =40;
 
-    resizeBtn.addEventListener('click', ()=>{
-        gridSize = fetchSize(length);
-        if(gridSize!=0 && gridSize != "")
-        {
-        createGrid(gridSize);
-        }
-        else
-        {
-        createGrid(16);            
-        }
-    })
-    
+    createGrid(currentGridSize);
 
-    function createGrid (gridLength)
-    {
-        cleanBoard();
-        for (let i = 0; i<gridLength; i++)
-    {
-        for(let j = 0; j<gridLength; j++)
-        {
-        const gridElement = document.createElement('div');
-        gridElement.style.backgroundColor = 'white';
-        gridElement.style.boxSizing = 'border-box';
-        gridElement.style.flexBasis = `${100/gridLength}%`;
-        // gridElement.style.border = '1px solid green';
-        mainContainer.appendChild(gridElement);
+    let isDrawing = false;
 
+        resizeBtn.addEventListener('click', ()=>{
+            const gridSize = fetchSize(length);
+            if(isNaN(gridSize))
+            {
+                alert("Please enter a number");
+            }
+            else if(gridSize>100)
+            {
+            alert("Please don't exceed 100x100 cells");
+            createGrid(currentGridSize);
+            }
+            else if(gridSize==1)
+            {
+            alert("1x1 is off limits try 2");
+            createGrid(currentGridSize);
+            }
+            else if (gridSize!=0&&gridSize!="")
+            {
+                currentGridSize = gridSize;
+                createGrid(currentGridSize);
+            }
+        })
+        
+
+        function createGrid (gridLength)
+        {
+            cleanBoard();
+            for (let i = 0; i<gridLength; i++)
+        {
+            for(let j = 0; j<gridLength; j++)
+            {
+            const gridElement = document.createElement('div');
+            gridElement.style.backgroundColor = 'white';
+            gridElement.style.boxSizing = 'border-box';
+            gridElement.style.flexBasis = `${100/gridLength}%`;
+            gridElement.style.border = '1px solid green';
+            mainContainer.appendChild(gridElement);
+
+            }
         }
     }
-}
 
-function cleanBoard()
-{
-    mainContainer.innerHTML = '';
-}
+    
 
-function fetchSize(ele)
+
+    function cleanBoard()
     {
-    const inputSize = ele.value;
-    ele.value ="";
-    currentGridSize =inputSize;
-    return inputSize;
-};
+        mainContainer.innerHTML = '';
+    }
 
-mainContainer.addEventListener('mouseover', (item)=>
-{
-    item.target.style.cursor = "pointer";
-    item.target.style.backgroundColor = "black"; 
-})
+    function fetchSize(ele)
+        {
+        const inputSize = ele.value;
+        ele.value ="";
+        return inputSize;
+    };
 
-clearBtn.addEventListener('click',()=>
+    window.addEventListener('mousedown', ()=> isDrawing = true);
+    window.addEventListener('mouseup', ()=> isDrawing = false);
+
+    mainContainer.addEventListener('mouseover', (item)=>
     {
-        if (currentGridSize!="")
-            createGrid(currentGridSize)
-    });
+        if (isDrawing)
+        {
+        item.target.style.cursor = "pointer";
+        item.target.style.backgroundColor = "black";
+        }
+
+    })
+
+    clearBtn.addEventListener('click',()=>
+        {
+            if (currentGridSize!="")
+                createGrid(currentGridSize)
+        });
